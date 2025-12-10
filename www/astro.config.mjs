c52ff9b4cee371c432c6936323a98f79b996ed1e
Chankoa <chandra_josephus@hotmail.com>
2025-12-10 16:58:18 +0100

    modifs annulées

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-const SITE = process.env.SITE ?? process.env.BASE_URL ?? 'https://example.com/www';
-const siteUrl = new URL(SITE);
-const basePath = siteUrl.pathname === '/' ? '/www' : siteUrl.pathname.replace(/\/$/, '');
-
 export default defineConfig({
-  site: siteUrl.origin + siteUrl.pathname,
-  base: basePath,
   integrations: [tailwind()],
   srcDir: 'src',
   server: {
